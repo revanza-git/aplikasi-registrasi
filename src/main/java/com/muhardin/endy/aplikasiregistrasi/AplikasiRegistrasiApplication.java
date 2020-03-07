2432ad5bbd7d2f39c09f448116d45b99511073fd
Endy Muhardin <endy@muhardin.com>
2020-03-07 22:38:46 +0700

    implement verifikasi email

--- src/main/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplication.java
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplication.java
@@ -1,10 +1,20 @@
 package com.muhardin.endy.aplikasiregistrasi;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.MustacheFactory;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class AplikasiRegistrasiApplication {
@@ -12,6 +22,11 @@ public class AplikasiRegistrasiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AplikasiRegistrasiApplication.class, args);
 	}
+
+	private static final String CLIENT_SECRET_JSON_FILE = "client_secret.json";
+
+	@Value("${gmail.folder}")
+	private String dataStoreFolder;
 
 	@Bean
 	public LayoutDialect layoutDialect() {
@@ -22,4 +37,21 @@ public class AplikasiRegistrasiApplication {
 	public SpringDataDialect springDataDialect() {
 		return new SpringDataDialect();
 	}
+
+	@Bean
+	public GoogleClientSecrets localFileClientSecrets() throws Exception {
+		return GoogleClientSecrets.load(jsonFactory(),
+				new InputStreamReader(new FileInputStream(dataStoreFolder + File.separator + CLIENT_SECRET_JSON_FILE)));
+	}
+
+	@Bean
+	public JsonFactory jsonFactory(){
+		return JacksonFactory.getDefaultInstance();
+	}
+
+	@Bean
+	public MustacheFactory mustacheFactory(){
+		return new DefaultMustacheFactory();
+	}
+
 }
--- src/main/java/com/muhardin/endy/aplikasiregistrasi/service/RegistrationService.java
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/service/RegistrationService.java
@@ -1,5 +1,7 @@
 package com.muhardin.endy.aplikasiregistrasi.service;
 
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
 import com.muhardin.endy.aplikasiregistrasi.dao.PesertaDao;
 import com.muhardin.endy.aplikasiregistrasi.dao.VerifikasiEmailDao;
 import com.muhardin.endy.aplikasiregistrasi.entity.Peserta;
@@ -9,7 +11,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.Valid;
+import java.io.StringWriter;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service @Transactional
@@ -18,8 +24,16 @@ public class RegistrationService {
     @Value("${token.expiry.days}")
     private Integer tokenExpiryDays;
 
+    @Value("${server.url}")
+    private String serverUrl;
+
+    @Value("${gmail.account.username}")
+    private String mailFrom;
+
     @Autowired private PesertaDao pesertaDao;
     @Autowired private VerifikasiEmailDao verifikasiEmailDao;
+    @Autowired private GMailApiService gMailApiService;
+    @Autowired private MustacheFactory mustacheFactory;
 
     public void registrasiPesertaBaru(Peserta p) {
         VerifikasiEmail ve = new VerifikasiEmail();
@@ -29,6 +43,26 @@ public class RegistrationService {
 
         pesertaDao.save(p);
         verifikasiEmailDao.save(ve);
+
+        kirimVerifikasi(ve);
+    }
+
+    private void kirimVerifikasi(VerifikasiEmail ve) {
+        Mustache templateEmail =
+                mustacheFactory.compile("templates/notification/verification.html");
+        Map<String, String> data = new HashMap<>();
+        data.put("nama", ve.getPeserta().getNama());
+        data.put("server.url", serverUrl);
+        data.put("token", ve.getToken());
+
+        StringWriter output = new StringWriter();
+        templateEmail.execute(output, data);
+
+        gMailApiService.kirimEmail(
+                mailFrom,
+                ve.getPeserta().getEmail(),
+                "Verifikasi Email "+ve.getPeserta().getNama(),
+                output.toString());
     }
 
     public void verifikasiToken(String token) {
