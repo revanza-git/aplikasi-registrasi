2e0e1fb88a047211f2e710e28235daf0e3dfd32b
Endy Muhardin <endy@muhardin.com>
2020-03-07 15:11:12 +0700

    skema database dan mapping

---
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/entity/Peserta.java
@@ -0,0 +1,33 @@
+package com.muhardin.endy.aplikasiregistrasi.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class Peserta {
+    @Id @Column(length = 36)
+    @GeneratedValue(generator = "uuid" )
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 2, max = 255)
+    private String nama;
+
+    @NotEmpty
+    @Size(min = 2, max = 255)
+    @Email
+    private String email;
+
+    @NotNull
+    private Boolean emailTerverifikasi = false;
+}
---
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/entity/VerifikasiEmail.java
@@ -0,0 +1,30 @@
+package com.muhardin.endy.aplikasiregistrasi.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class VerifikasiEmail {
+    @Id @Column(length = 36)
+    @GeneratedValue(generator = "uuid" )
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_peserta")
+    private Peserta peserta;
+
+    @NotNull
+    @NotEmpty
+    private String token;
+
+    @NotNull
+    private LocalDateTime expire;
+}
