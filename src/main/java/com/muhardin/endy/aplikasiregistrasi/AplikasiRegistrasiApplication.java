d3003e9df13aaa2034e4ba9205757a83ec704a6a
Endy Muhardin <endy@muhardin.com>
2020-03-07 13:44:46 +0700

    memasang template bootstrap

--- src/main/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplication.java
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplication.java
@@ -1,7 +1,9 @@
 package com.muhardin.endy.aplikasiregistrasi;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AplikasiRegistrasiApplication {
@@ -10,4 +12,8 @@ public class AplikasiRegistrasiApplication {
 		SpringApplication.run(AplikasiRegistrasiApplication.class, args);
 	}
 
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
+	}
 }
