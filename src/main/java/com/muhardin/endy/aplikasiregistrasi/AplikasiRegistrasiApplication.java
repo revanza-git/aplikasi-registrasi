6c1dce03c6335d320bd5c9aa8f52c7e9df8eca5a
Endy Muhardin <endy@muhardin.com>
2020-03-07 10:11:36 +0700

    Initial commit

---
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.aplikasiregistrasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiRegistrasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiRegistrasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muhardin/endy/aplikasiregistrasi/AplikasiRegistrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.aplikasiregistrasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiRegistrasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
