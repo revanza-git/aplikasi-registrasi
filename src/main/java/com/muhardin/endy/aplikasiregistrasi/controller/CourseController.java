ea3e40ca7e3944ca3828c10c5a86be905173c437
Endy Muhardin <endy@muhardin.com>
2020-03-07 16:58:11 +0700

    implement enroll page

--- src/main/java/com/muhardin/endy/aplikasiregistrasi/controller/CourseController.java
+++ src/main/java/com/muhardin/endy/aplikasiregistrasi/controller/CourseController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -30,8 +31,8 @@ public class CourseController {
     }
 
     @GetMapping("/enroll")
-    public ModelMap displayEnrollment() {
-        return new ModelMap();
+    public ModelMap displayEnrollment(@RequestParam Materi materi) {
+        return new ModelMap().addAttribute("materi", materi);
     }
 
     @PostMapping("/enroll")
